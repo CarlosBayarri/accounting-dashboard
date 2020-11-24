623861af068c7aaba3f9481322895ef5b5693dd4
carlosbayarri <carlobr7@gmail.com>
2020-11-24 09:12:46 +0100

    sidebar to navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
@@ -8,15 +10,23 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: []
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit, OnDestroy {
 
   username: string;
   userSubscription: Subscription;
+  activeLink: string = 'home';
+  isScrolling: boolean = false;
 
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<AppState>, private AuthService: AuthService, private router: Router) { }
 
+  logOut() {
+    this.AuthService.logOut().then(() => {
+      this.router.navigate(['/login']);
+    })
+  }
+  
   ngOnInit() {
     this.userSubscription = this.store.select('user').pipe(filter(({user}) => user !== null)).subscribe(({user}) => {
       this.username = user.name;
@@ -26,4 +36,15 @@ export class NavbarComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.userSubscription.unsubscribe();
   }
+
+  @HostListener('body:scroll', ['$event'])
+  onScroll(event) {
+    if (event.srcElement.scrollTop > 62) {
+      this.isScrolling = true;
+    } else {
+      this.isScrolling = false;
+
+    }
+  }
+  
 }
